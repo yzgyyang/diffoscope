3217c158793e08b198c9c394f72bc54768c3e184
Chris Lamb <lamby@debian.org>
2017-09-08 16:21:08 +0100

    test_cpio: PEP8 file.
    Dch-Gbp: ignore

--- tests/comparators/test_cpio.py
+++ tests/comparators/test_cpio.py
@@ -25,26 +25,30 @@ from ..utils.data import load_fixture, get_data
 from ..utils.tools import skip_unless_tools_exist
 from ..utils.nonexisting import assert_non_existing
 
-
 cpio1 = load_fixture('test1.cpio')
 cpio2 = load_fixture('test2.cpio')
+
 
 def test_identification(cpio1, cpio2):
     assert isinstance(cpio1, CpioFile)
     assert isinstance(cpio2, CpioFile)
 
+
 def test_no_differences(cpio1):
     difference = cpio1.compare(cpio1)
     assert difference is None
+
 
 @pytest.fixture
 def differences(cpio1, cpio2):
     return cpio1.compare(cpio2).details
 
+
 @skip_unless_tools_exist('cpio')
 def test_listing(differences):
     expected_diff = get_data('cpio_listing_expected_diff')
     assert differences[0].unified_diff == expected_diff
+
 
 @skip_unless_tools_exist('cpio')
 def test_symlink(differences):
@@ -53,12 +57,14 @@ def test_symlink(differences):
     expected_diff = get_data('symlink_expected_diff')
     assert differences[1].unified_diff == expected_diff
 
+
 @skip_unless_tools_exist('cpio')
 def test_compressed_files(differences):
     assert differences[2].source1 == 'dir/text'
     assert differences[2].source2 == 'dir/text'
     expected_diff = get_data('text_ascii_expected_diff')
     assert differences[2].unified_diff == expected_diff
+
 
 @skip_unless_tools_exist('cpio')
 def test_compare_non_existing(monkeypatch, cpio1):
