41075d793f9f6cffdde176b61b8f6df144170648
Chris Lamb <lamby@debian.org>
2018-01-11 12:41:39 +0530

    comparators.utils.specialize: Ensure block comment starts with '# '.

--- diffoscope/comparators/utils/specialize.py
+++ diffoscope/comparators/utils/specialize.py
@@ -32,7 +32,7 @@ def try_recognize(file, cls, recognizes):
 
     # Does this file class match?
     with profile('recognizes', file):
-        #logger.debug("trying %s on %s", cls, file)
+        # logger.debug("trying %s on %s", cls, file)
         if not recognizes(file):
             return False
 
