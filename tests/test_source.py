e41abe00d170462081abdf9855aa181512f791da
Chris Lamb <lamby@debian.org>
2018-03-03 21:48:47 +0000

    Move DOS/MBR check into the testsuite.

---
+++ tests/test_source.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# diffoscope: in-depth comparison of files, archives, and directories
+#
+# Copyright © 2018 Chris Lamb <lamby@debian.org>
+#
+# diffoscope is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# diffoscope is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with diffoscope.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+import glob
+import pytest
+import diffoscope
+
+BASE_DIR = os.path.dirname(os.path.abspath(diffoscope.__file__))
+
+
+def test_dos_mbr():
+    for x in glob.iglob(os.path.join(BASE_DIR, '**', '*.py'), recursive=True):
+        assert 'DOS/MBR' not in open(x).read()
