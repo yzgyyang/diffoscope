aca61db5863bc2ba5cf507a4fb9687f2cbee9b41
Mattia Rizzolo <mattia@debian.org>
2018-03-08 12:18:32 +0100

    tests: test_dos_mbr: explicitly use utf8 for reading files
    Obviously, UnicodeDecodeErrors are right behind you.
    
    Gbp-Dch: Ignore
    Signed-off-by: Mattia Rizzolo <mattia@debian.org>

--- tests/test_source.py
+++ tests/test_source.py
@@ -26,4 +26,5 @@ BASE_DIR = os.path.dirname(os.path.abspath(diffoscope.__file__))
 
 def test_dos_mbr():
     for x in glob.iglob(os.path.join(BASE_DIR, '**', '*.py'), recursive=True):
-        assert 'DOS/MBR' not in open(x).read()
+        with open(x, encoding='utf-8', errors='replace') as f:
+            assert 'DOS/MBR' not in f.read()
