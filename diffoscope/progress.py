yzgyyang/diffoscope

commit dfb0c190173dff953fe1481797486d0b3e09306e

Clear the progress bar after completion. (Closes: #901758)
Handle terminals that do not support erasing the line by
filling the terminal with spaces.

Ignore output devices that are not terminals.

Implements: https://bugs.debian.org/901758
Requires: Python 3.2 for shutil.get_terminal_size

diffoscope/progress.py
@@ -3,6 +3,7 @@
 # diffoscope: in-depth comparison of files, archives, and directories
 #
 # Copyright © 2016 Chris Lamb <lamby@debian.org>
+# Copyright © 2018 Paul Wise <pabs@debian.org>
 #
 # diffoscope is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,6 +21,7 @@
 import os
 import sys
 import json
+import signal
 import logging
 
 logger = logging.getLogger(__name__)
@@ -215,9 +217,37 @@ class ProgressBar(object):
                 # Remove after https://github.com/niltonvolpato/python-progressbar/pull/57 is fixed.
                 kwargs.setdefault('fd', sys.stderr)
                 super().__init__(*args, **kwargs)
+                # Terminal handling after parent init since that sets self.fd
+                if self.fd.isatty():
+                    from curses import tigetstr, setupterm
+                    setupterm()
+                    self.erase_to_eol = tigetstr('el')
+                else:
+                    self.erase_to_eol = None
 
             def _need_update(self):
                 return True
+
+            def erase_line(self):
+                if self.erase_to_eol:
+                    self.fd.buffer.write(self.erase_to_eol)
+                elif self.fd.isatty():
+                    from shutil import get_terminal_size
+                    width = get_terminal_size().columns
+                    print(end='\r', file=self.fd)
+                    print(' ' * width, end='', file=self.fd)
+                else:
+                    # Do not flush if nothing was written
+                    return
+                self.fd.flush()
+
+            def finish(self):
+                self.finished = True
+                self.update(self.maxval)
+                # Clear the progress bar after completion
+                self.erase_line()
+                if self.signal_set:
+                    signal.signal(signal.SIGWINCH, signal.SIG_DFL)
 
         self.bar = OurProgressBar(widgets=(
             ' ',

commit 81e54147939d1437f80995a94b5f4f8f7f0dbbb6

Use the ProgressBar's idea of the terminal width instead
More likely to overwrite the correct number of characters.

Fixes: commit dfb0c190173dff953fe1481797486d0b3e09306e

diffoscope/progress.py
@@ -232,10 +232,8 @@ class ProgressBar(object):
                 if self.erase_to_eol:
                     self.fd.buffer.write(self.erase_to_eol)
                 elif self.fd.isatty():
-                    from shutil import get_terminal_size
-                    width = get_terminal_size().columns
                     print(end='\r', file=self.fd)
-                    print(' ' * width, end='', file=self.fd)
+                    print(' ' * self.term_width, end='', file=self.fd)
                 else:
                     # Do not flush if nothing was written
                     return