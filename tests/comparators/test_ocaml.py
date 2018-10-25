554c9a20b9904ca27183688b2fc8c5775e9ce9cb
Chris Lamb <lamby@debian.org>
2018-10-25 09:27:31 -0400

    Prevent test failures when running under stretch-backports by checking the ocaml version number. (Closes: #911846)

--- tests/comparators/test_ocaml.py
+++ tests/comparators/test_ocaml.py
@@ -18,15 +18,24 @@
 # along with diffoscope.  If not, see <https://www.gnu.org/licenses/>.
 
 import pytest
+import subprocess
 
 from diffoscope.comparators.ocaml import OcamlInterfaceFile
 
 from ..utils.data import load_fixture, get_data
-from ..utils.tools import skip_unless_tools_exist
+from ..utils.tools import skip_unless_tool_is_at_least
 from ..utils.nonexisting import assert_non_existing
 
 cmi1 = load_fixture('test1.cmi')
 cmi2 = load_fixture('test2.cmi')
+
+
+def ocaml_version():
+    try:
+        out = subprocess.check_output(['ocaml', '-version'])
+    except subprocess.CalledProcessError as e:
+        out = e.output
+    return out.decode('utf-8').split()[-1]
 
 
 def test_identification(cmi1):
@@ -43,12 +52,12 @@ def test_no_differences(cmi1):
     assert difference is None
 
 
-@skip_unless_tools_exist('ocamlobjinfo')
+@skip_unless_tool_is_at_least('ocamlobjinfo', ocaml_version, '4.05.0')
 def test_diff(differences):
     expected_diff = get_data('ocaml_expected_diff')
     assert differences[0].unified_diff == expected_diff
 
 
-@skip_unless_tools_exist('ocamlobjinfo')
+@skip_unless_tool_is_at_least('ocamlobjinfo', ocaml_version, '4.05.0')
 def test_compare_non_existing(monkeypatch, cmi1):
     assert_non_existing(monkeypatch, cmi1, has_null_source=False)
