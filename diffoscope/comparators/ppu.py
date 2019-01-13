yzgyyang/diffoscope

commit f0ae5eba851ee4a46a649c03fbc4875d90898552

comparators.ppu: Use file_header to simplify magic detection and version parsing.

diffoscope/comparators/ppu.py
@@ -65,11 +65,12 @@ class PpuFile(File):
     def recognizes(cls, file):
         if not super().recognizes(file):
             return False
-        with open(file.path, 'rb') as f:
-            magic = f.read(3)
-            if magic != b"PPU":
-                return False
-            ppu_version = f.read(3).decode('ascii', errors='ignore')
+
+        if file.file_header.startswith(b'PPU'):
+            return False
+
+        ppu_version = f.file_header[3:6].decode('ascii', errors='ignore')
+
         if not hasattr(PpuFile, 'ppu_version'):
             try:
                 with profile('command', 'ppudump'):

commit 248e9baa327091e876d5379098310a4ec3e3c473

comparators.ppu: Fix inverted logic and invalid reference to "file".
These were regressions introduced in f0ae5eba85.

diffoscope/comparators/ppu.py
@@ -66,10 +66,10 @@ class PpuFile(File):
         if not super().recognizes(file):
             return False
 
-        if file.file_header.startswith(b'PPU'):
+        if not file.file_header.startswith(b'PPU'):
             return False
 
-        ppu_version = f.file_header[3:6].decode('ascii', errors='ignore')
+        ppu_version = file.file_header[3:6].decode('ascii', errors='ignore')
 
         if not hasattr(PpuFile, 'ppu_version'):
             try: